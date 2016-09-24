60d59b68809a3095b6bb8dc773f84279bd03b079
patrickbeyer85@gmail.com <patrickbeyer85@gmail.com>
2016-09-24 22:59:21 +0200

    SG-009 Display explosion as an animation. Make draw context a singleton. Refactor draw methods.

--- spaceGame.js
+++ spaceGame.js
@@ -1,6 +1,7 @@
 
 //global definitions
 var TO_RADIANS = Math.PI/180;
+var refreshRate = 33; //in milliseconds
 var globalWidth = 1000;
 var globalHeight = 800;
 var winScore = 5;
@@ -8,8 +9,28 @@ var audioExplosion = new Audio('public/explosion01.m4a');
 var audioShot = new Audio('public/shoot01.m4a');
 audioExplosion.playbackRate = 2.0;
 
-var ship1 = new SpaceShip(10,10,30,40,180, 'player1');
-var ship2 = new SpaceShip(960,750,30,40,0, 'player2');
+var ship1 = new SpaceShip(10,10,30,40,180, 'player1', 'bgbattleship');
+var ship2 = new SpaceShip(960,750,30,40,0, 'player2', 'bgspeedship');
+
+//provide draw context as a singleton
+var Context2D = (function(){
+    var instance;
+    function createInstance(){
+        var cvs = document.getElementById("myCanvas");
+        var context = cvs.getContext("2d");
+        return context;
+    }
+
+    return {
+        getInstance: function(){
+            if(!instance){
+                return createInstance()
+            } else {
+                return instance;
+            }
+        }
+    }
+})();
 
 function Vector2D(x,y) {
     this.x = x;
@@ -91,7 +112,7 @@ Shot.prototype.middle = function () {
     return new Vector2D(this.position.x+this.width/2, this.position.y+this.height/2);
 };
 
-function SpaceShip(x, y, width, height, rotation, playerName) {
+function SpaceShip(x, y, width, height, rotation, playerName, imageName) {
     this.position = new Vector2D(x,y);
     this.speed = new Vector2D(0,0);
     this.width = width;
@@ -104,6 +125,8 @@ function SpaceShip(x, y, width, height, rotation, playerName) {
     this.shot = new Shot(-32,-32,16,16,0);
     this.score = 0;
     this.playerName = playerName;
+    this.imageName = imageName;
+    this.animation = new ScaleAnimation(200, 211, 300, 100);
 }
 
 SpaceShip.prototype.middle = function () {
@@ -186,6 +209,7 @@ SpaceShip.prototype.giveFire = function () {
 SpaceShip.prototype.explode = function () {
     this.alive = false;
     audioShot.play();
+    this.animation.status = 'started'; //start animation;
 }
 
 function collisionCheck(obj1, obj2) {  // obj must have width, height, position=vector2D
@@ -244,8 +268,8 @@ function respawn(ship1, ship2){
             ship2.rotation = 0;
             ship1.alive = true;
             ship2.alive = true;
-            shipX.shot.active = true;
-            shipY.shot.active = true;
+            ship1.shot.active = true;
+            ship2.shot.active = true;
     }
     }, 1000)    
 }
@@ -260,27 +284,96 @@ function winGame() {
 
 }
 
-function drawObject(ctx, img, object) {
-    ctx.save();
+function ScaleAnimation(width, height, timeToScaleUp, timeToScaleDown){
+    this.status = 'ended'; //can be 'ended', 'started', 'paused'
+    this.timeSinceStart = 0; //in ms
+    this.height = height; //max height
+    this.width = width; //max width
+    this.currentHeight = 10;
+    this.currentWidth = 10;
+    this.timeToScaleDown = timeToScaleDown;
+    this.timeToScaleUp = timeToScaleUp;
+
+    this.step = function(){
+        if(this.status != 'ended'){
+            this.timeSinceStart += refreshRate;
+
+            if(this.timeSinceStart < timeToScaleUp){
+                var stepsToScaleUp = this.timeToScaleUp/refreshRate
+                var scaleUpStepSizeForWidth = this.width/stepsToScaleUp;
+                var scaleUpStepSizeForHeight = this.height/stepsToScaleUp;
+
+                this.currentHeight += scaleUpStepSizeForHeight;
+                this.currentWidth += scaleUpStepSizeForWidth;
+            } 
+            else if(this.timeSinceStart < timeToScaleUp + timeToScaleDown){
+                var stepsToScaleDown = this.timeToScaleDown/refreshRate
+                var scaleDownStepSizeForWidth = this.currentWidth/stepsToScaleDown;
+                var scaleDownStepSizeForHeight = this.currentHeight/stepsToScaleDown;
+
+                this.currentHeight -= scaleDownStepSizeForHeight;
+                this.currentWidth -= scaleDownStepSizeForWidth;
+
+                if(this.currentHeight < 0 ){
+                    this.currentHeight = 0;
+                }
+                if(this.currentWidth < 0 ){
+                    this.currentWidth = 0;
+                }
+            } else {
+                this.status = 'ended';
+                this.timeSinceStart = 0;
+            }
+        }
+    }
+}
+
+function drawObject(img, object, width, height) {
+    var widthToDraw = typeof width == 'undefined' ? object.width : width;
+    var heightToDraw = typeof height == 'undefined' ? object.height : height;
     var mid = object.middle();
+    var ctx = Context2D.getInstance();
+
+    ctx.save();
     ctx.translate(mid.x, mid.y);
     ctx.rotate(object.rotation * TO_RADIANS);
-    ctx.drawImage(img, -object.width/2, -object.height/2, object.width, object.height);
+    ctx.drawImage(img, -object.width/2, -object.height/2, widthToDraw, heightToDraw);
     ctx.restore();
 }
 
+//this animation draws the img initially with size 0 and scales it up
+//(in 'timeToScaleUp' ms) to its maximum size
+//to scale it down afterwards in 'timeToScaleDown' milliseconds.
+//If timeToScaleDown is missing it will not scale down.
+function drawScaleAnimation(img, object) {
+    var ctx = Context2D.getInstance();
+
+    object.animation.step();
+
+    if(object.animation.status == 'started'){
+        drawObject(img, object, object.animation.currentWidth, object.animation.currentHeight);
+    }
+}
+
+function drawShip(ship) {
+
+    var img = document.getElementById(ship.imageName);
+    var imgExplosion = document.getElementById("explosion");
+    var ctx = Context2D.getInstance();
+
+    if(ship.alive){
+        drawObject(img, ship);
+    } else {
+        drawScaleAnimation(imgExplosion, ship);
+    }
+}
 
 window.onload = function() {
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-
-    var img1=document.getElementById("bgbattleship");
-    var img2=document.getElementById("bgspeedship");
     var shot1 = document.getElementById("shot01");
     var shot2 = document.getElementById("shot02");
-    var explosion = document.getElementById("explosion");
     
     function step(){
+        var ctx = Context2D.getInstance();
         ctx.fillStyle="#FFFFFF";
         ctx.fillRect(0,0,globalWidth,globalHeight);
 
@@ -293,16 +386,16 @@ window.onload = function() {
         shipCollisionCheck(ship2, ship1);
         winGame();
         //view
-        drawObject(ctx, ship1.alive ? img1 : explosion, ship1);
-        drawObject(ctx, ship2.alive ? img2 : explosion, ship2);
+        drawShip(ship1);
+        drawShip(ship2);
         if (ship1.shot.active) {
-            drawObject(ctx, shot1, ship1.shot);
+            drawObject(shot1, ship1.shot);
         }
         if (ship2.shot.active) {
-            drawObject(ctx, shot2, ship2.shot);
+            drawObject(shot2, ship2.shot);
         }
     }
-    setInterval(step,33);
+    setInterval(step, refreshRate);
 
     document.getElementById('body').addEventListener('keydown', function (e) {
         var key = e.which || e.keyCode;
